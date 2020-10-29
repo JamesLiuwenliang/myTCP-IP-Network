b3d032e98e8f2847f938f7f6a66a14c68ca1c4a6
JamesLiuwenliang <13114817178@163.com>
2020-10-30 01:02:58 +0900

    mutex

--- Part02_Linux-Network/chap18_Thread/thread4.c
+++ Part02_Linux-Network/chap18_Thread/thread4.c
@@ -4,31 +4,65 @@
 #include <unistd.h>
 /*因为共用sum空间,可能会有临界区风险*/
 #define NUM_THREAD 100
-void *thread_summation(void *arg);
-int sum = 0;
+void *thread_inc(void *arg);
+void *thread_des(void *arg);
+long long num = 0;
+
+pthread_mutex_t mutex;
+
+/*
+ * 互斥量：通过加锁，解决线程同步问题
+ */
+
 int main(int argc ,char *argv[]){
-    pthread_t id_t1,id_t2;
-    int range1[] = {1,5};
-    int range2[] = {6,10};
 
-    pthread_create(&id_t1,NULL,thread_summation , (void *)range1);
-    pthread_create(&id_t2,NULL,thread_summation , (void *)range2);
+    pthread_t thread_id[NUM_THREAD];
 
-    pthread_join(id_t1, NULL);
-    pthread_join(id_t2, NULL);
-
-    printf("result : %d\n",sum);
-}
-
-void *thread_summation(void *arg){
     int i;
 
-    int startNum = ((int *)arg)[0];
-    int endNum = ((int *)arg)[1];
+    // 声明了保存互斥量读取值的变量
+    pthread_mutex_init(&mutex,NULL);
 
-    while(startNum <= endNum){
-        sum += startNum++;
+    for(i=0;i<NUM_THREAD;i++){
+        if(i%2){
+            pthread_create(&(thread_id[i]) ,NULL, thread_inc,NULL);
+        }else{
+            pthread_create(&(thread_id[i]), NULL, thread_des,NULL);
+        }       
     }
+
+    for(i=0;i<NUM_THREAD;i++){
+        pthread_join(thread_id[i],NULL);
+    }
+
+    printf("result : %lld\n",num);
+
+    // 销毁互斥量
+    pthread_mutex_destroy(&mutex);
+    return 0;
+}
+
+
+// 应最大限度的减少调用锁的次数,thread_dex()明显比thread_inc()调用次数要多很多次,所以运行次数很慢
+void *thread_inc(void *arg){
+
+    int i;
+    pthread_mutex_lock(&mutex);
+    for(i=0;i<500000;i++){
+        num += 1;
+    }
+    pthread_mutex_unlock(&mutex);
+    return NULL;
+}
+void *thread_des(void *arg){
+    int i;
+    
+    for(i=0;i<500000;i++){
+        pthread_mutex_lock(&mutex);
+        num -= 1;
+        pthread_mutex_unlock(&mutex);
+    }
+    return NULL;
 
 
 }
